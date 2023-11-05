7c955bf243664385553b703c9de62b831eefadff
lifei76 <lifei76@meituan.com>
2023-11-05 21:55:16 +0800

    inet, inet_sw 的含义解释. 这对理解mptcp协议是必须要知道

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -2074,7 +2074,7 @@ out_unregister_tcp_proto:
 	goto out;
 }
 
-// inet, inet_sw, 自定义动态协议(mptcp)的说明文章: https://ieevee.com/tech/2017/06/12/kernel-network-stack.html
+// inet, inet_sw 的含义解释. 这对理解mptcp协议是必须要知道!  https://ieevee.com/tech/2017/06/12/kernel-network-stack.html
 fs_initcall(inet_init);     // [网络子系统初始化]  协议栈初始化的入口
 
 /* ------------------------------------------------------------------------ */
